6f000edf570d82043b744ae357f595c1d469fa70
Dennis <dennis@vonbargen.se>
2013-09-10 17:59:26 +0200

    initial project version

---
+++ tests/TestConsole.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import ShapeChecker.*;
+
+
+public class TestConsole {
+
+	@Test
+	public void test() {
+		ShapeConsole s = new ShapeConsole();
+	}
+}
